28b4f9aed85411c5049bdff02be7103bf547073d
Chen Liang <chenlian@andrew.cmu.edu>
2015-01-27 02:31:19 -0500

    fix

---
+++ searchLoctionIDInsideChina.py
@@ -0,0 +1,84 @@
+from temboo.core.session import TembooSession
+from temboo.Library.Instagram import SearchLocations
+import numpy as np
+import time
+import os
+import json
+
+def getLocationIDsInsideMainlandChina():
+    locationIDs = set()
+
+    # Create a session with your Temboo account details
+    session = TembooSession("chenlian", "myFirstApp", "51dc1a9d42e34bcf90a97d6c8bb7ce6e")
+
+    # Instantiate the Choreo
+    searchLocationsChoreo = SearchLocations(session)
+
+    # Get an InputSet object for the Choreo
+    searchLocationsInputs = searchLocationsChoreo.new_input_set()
+
+    # Get latitudes and longitudes in mainland China
+    latsLongsInMainland = getMainlandChinaLatitudeAndLongitude()
+    for lat in latsLongsInMainland:
+        for lon in latsLongsInMainland[lat]:
+            # Set the Choreo inputs
+            searchLocationsInputs.set_Distance("5000")
+            searchLocationsInputs.set_Latitude(str(lat))
+            searchLocationsInputs.set_Longitude(str(lon))
+            searchLocationsInputs.set_ClientID("dc9bbb4e063e434a9baf0933923a973c")
+
+            # Execute the Choreo
+            searchLocationsResults = searchLocationsChoreo.execute_with_results(searchLocationsInputs)
+
+            # Get the Choreo outputs
+            IDOutput = json.loads(searchLocationsResults.get_Response())
+            # print len(IDOutput["data"])
+            if len(IDOutput["data"]) == 0:
+                continue
+            for i in xrange(len(IDOutput["data"])):
+                locationIDs.add(IDOutput["data"][i]["id"])
+                print IDOutput["data"][i]["name"]
+            start = time.time()
+            elapedTime = 0.2
+            while time.time() - start < elapedTime:
+                continue
+    print locationIDs
+    return locationIDs
+
+def getMainlandChinaLatitudeAndLongitude():
+    mapOfMainland = dict()
+
+    for lat in np.arange(40.212, 39.735, -0.04):  # Beijing
+        mapOfMainland[lat] = np.arange(116.096, 116.620, 0.04)
+    for lat in np.arange(31.379, 30.787, -0.04):  # Shanghai
+        mapOfMainland[lat] = np.arange(121.130, 121.815, 0.04)
+    for lat in np.arange(23.464, 22.524, -0.04):  # Guangzhou
+        mapOfMainland[lat] = np.arange(113.147, 114.064, 0.04)
+    return mapOfMainland
+
+def writeFile(filename, contents, mode="wt"):
+    # wt = "write text"
+    with open(filename, mode) as fout:
+        fout.write(contents)
+
+def writeLocationIDs():
+    path = "data" + os.sep + "locationIDs.txt"
+
+    # create the temp dir, if it is not there
+    if (not os.path.exists("data")):
+        os.makedirs("data")
+
+    # remove old file, if it is there
+    if (os.path.exists(path)):
+        os.remove(path)
+
+    contents = ""
+
+    locationIDs = getLocationIDsInsideMainlandChina()
+    for id in locationIDs:
+        contents += str(id) + " "
+
+    # add new file
+    writeFile(path, contents)
+
+writeLocationIDs()
---
+++ userOverTime.py
@@ -0,0 +1,81 @@
+import os
+import datetime
+import time
+from temboo.Library.Instagram import GetRecentMediaForLocation
+from temboo.core.session import TembooSession
+
+def readFile(filename, mode="rt"):
+    # rt = "read text"
+    with open(filename, mode) as fin:
+        return fin.read()
+
+def readLocationIDs():
+    path = "data" + os.sep + "locationIDs.txt"
+    contents = readFile(path)
+    IDList = contents.split()
+    return IDList
+
+# helper function to obtain UNIX timestamp
+def getUNIXTimestamp(year, month):
+    d = datetime.datetime(year, month, 0, 0, 0, 0, 0)
+    return time.mktime(d.timetuple())
+
+def getDateDistributionData():
+    # Create a session with your Temboo account details
+    session = TembooSession("chenlian", "myFirstApp", "51dc1a9d42e34bcf90a97d6c8bb7ce6e")
+
+    # Instantiate the Choreo
+    getRecentMediaForLocationChoreo = GetRecentMediaForLocation(session)
+
+    # Get an InputSet object for the Choreo
+    getRecentMediaForLocationInputs = getRecentMediaForLocationChoreo.new_input_set()
+
+    IDList = readLocationIDs()
+    totalMedia = dict()
+    for year in xrange(2011, 2015):
+        for month in xrange(1, 13):
+            timeStart = getUNIXTimestamp(year, month)
+            timeEnd = getUNIXTimestamp(year, month+1) if (month != 12) else getUNIXTimestamp(year+1, 1)
+            title = str(year) + "," + str(month)
+
+            totalMedia[title] = 0  # set the total amount of current month media to 0
+            for id in IDList:
+                # Set the Choreo inputs
+                getRecentMediaForLocationInputs.set_LocationID(id)
+                getRecentMediaForLocationInputs.set_MaxTimestamp(str(timeEnd))
+                getRecentMediaForLocationInputs.set_ClientID("dc9bbb4e063e434a9baf0933923a973c")
+                getRecentMediaForLocationInputs.set_MinTimestamp(str(timeStart))
+
+                # Execute the Choreo
+                getRecentMediaForLocationResults = getRecentMediaForLocationChoreo.execute_with_results(getRecentMediaForLocationInputs)
+
+                # As "latitude" just occur once in one post, we use this to count the amount of media of the month
+                totalMedia[title] += getRecentMediaForLocationResults.get_Response().count("latitude")
+    return totalMedia
+
+def writeMediaOverDate():
+    path = "data" + os.sep + "MediaAmountOverDate.txt"
+
+    # create the temp dir, if it is not there
+    if (not os.path.exists("data")):
+        os.makedirs("data")
+
+    # remove old file, if it is there
+    if (os.path.exists(path)):
+        os.remove(path)
+
+    contents = ""
+
+    dateMediaData = getDateDistributionData()
+    for date in dateMediaData:
+        contents += date + ": " + str(dateMediaData[date])
+
+    # add new file
+    writeFile(path, contents)
+
+def writeFile(filename, contents, mode="wt"):
+    # wt = "write text"
+    with open(filename, mode) as fout:
+        fout.write(contents)
+
+writeMediaOverDate()
